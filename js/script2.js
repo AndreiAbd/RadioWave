4277ac9b3c3ec8dda476bd6be35d6b2f77f41483
CoderX <73896715+AndreiAbd@users.noreply.github.com>
2022-03-07 20:57:23 +0200

    CoderX

--- js/script2.js
+++ js/script2.js
@@ -13,38 +13,8 @@ Github    :  https://github.com/AndreiAbd
 */
 
 let
-    d = new Date(),
-    elem = document.documentElement;
+    d = new Date();
 
-//Reload
-function reload() {
-    document.getElementById('00_Click').play();
-    location.reload();
-}
-// Get Current Year
-document.getElementById("addCurrentYear").innerHTML = d.getFullYear();
-
-/* View in fullscreen */
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-        document.getElementById('00_Click').play();
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-}
-//Side Nave Bar
-function openNav() {
-    document.getElementById('00_Click').play();
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById('00_Click').play();
-    document.getElementById("mySidenav").style.width = "0";
-}
 //Loader Page
 window.addEventListener("load", function() {
     const loader = document.querySelector(".loader");
